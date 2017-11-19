b5de63f47ba3dfa052dd5c34d14ef2f8a36d68f9
ravi <rafael.vitor@jusbrasil.com.br>
2017-11-19 12:01:42 -0300

    Basic thread with user input controlled timer

--- Flight.java
+++ Flight.java
@@ -1,6 +1,25 @@
 import java.time.LocalDateTime;
+import java.util.TimerTask;
 
-public class Flight {
+public class Flight extends TimerTask {
+
+  private double height;
+
+  private Airport fromWhere;
+
+  private Airport toWhere;
+
+  private FlightStatus flightStatus;
+
+  public int getCounter() {
+    return counter;
+  }
+
+  public void setCounter(int counter) {
+    this.counter = counter;
+  }
+
+  private int counter;
 
   public Flight(String fromWhere, String toWhere) {
     this.fromWhere = new Airport(fromWhere);
@@ -10,13 +29,38 @@ public class Flight {
     System.out.println("at " + LocalDateTime.now());
 
   }
-  private double speed;
 
-  private double height;
+  public void selectNextState(String inp) {
+    System.out.println("select next state" + inp);
+    try {
+      Thread.sleep(6000);
+    } catch (InterruptedException e) {
+    }
+  }
 
-  private Airport fromWhere;
+  public void moveToNextState() {
+    System.out.println("moving to next state");
+    System.out.println("current state: " + counter);
+    System.out.println("next state: " + (counter + 1));
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+    }
 
-  private Airport toWhere;
+    this.counter = counter + 1;
+  }
 
-  private FlightStatus flightStatus;
+  @Override
+  public void run() {
+    System.out.println("Timer task started at:" + LocalDateTime.now());
+    moveToNextState();
+    System.out.println("Timer task finished at:" + LocalDateTime.now());
+
+  }
+
+  @Override
+  public String toString() {
+    System.out.println("printing super to string");
+    return super.toString();
+  }
 }
